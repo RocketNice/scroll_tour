87cd368aba0d6d55d452f9272168d0827c7d9686
RocketNice <lisdb@bk.ru>
2022-08-10 19:40:04 +0300

    scroll_tour

---
+++ scroller_tour.php
@@ -0,0 +1,53 @@
+<section class="popular_routes_container">
+    <p class='title_cards'>Популярные маршруты</p>
+    <div class="popular_routes" id="horizontal-scroller">
+    <div class="card--content"><img class='img_background'
+            src="http://nationalpark/wp-content/themes/alania-park/assets/img/RectangleFirst.png">
+        <div class='frame_card'>
+            <a class='text_card'>К ледниику Тана</a>
+            <div class='circles first_circle'>
+            </div>
+            <div class='circles second_circle'>
+            </div>
+            <div class='circles third_circle'>
+            </div>
+            <div class='circles fourth_circle'>
+            </div>
+            <div class='circles fifth_circle'>
+            </div>
+        </div>
+    </div>
+    <div class="card--content"><img class='img_background'
+            src="http://nationalpark/wp-content/themes/alania-park/assets/img/RectangleSecond.png">
+        <div class='frame_card'>
+            <a class='text_card'>К ледниику Тана</a>
+            <div class='circles first_circle'>
+            </div>
+            <div class='circles second_circle'>
+            </div>
+            <div class='circles third_circle'>
+            </div>
+            <div class='circles fourth_circle'>
+            </div>
+            <div class='circles fifth_circle'>
+            </div>
+        </div>
+    </div>
+    <div class="card--content"><img class='img_background'
+            src="http://nationalpark/wp-content/themes/alania-park/assets/img/RectangleFirst.png">
+        <div class='frame_card'>
+            <a class='text_card'>К ледниику Тана</a>
+            <div class='circles first_circle'>
+            </div>
+            <div class='circles second_circle'>
+            </div>
+            <div class='circles third_circle'>
+            </div>
+            <div class='circles fourth_circle'>
+            </div>
+            <div class='circles fifth_circle'>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
